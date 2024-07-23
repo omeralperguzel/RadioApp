7a09ea73b964e7cc96ee0c78c90edce8a7c92403
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2024-07-23 19:05:11 +0300

    Fix some issues in menu
    - Cleaned headers
    - Back button now works

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from "expo-router"
 const StackLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)"/>
+      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
     </Stack>
   );
 };
+
+export default StackLayout;
