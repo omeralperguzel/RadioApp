d75055da9b4cc904f271adf23621bb8adf2ceddb
Ömer Alper Güzel <90072860+omeralperguzel@users.noreply.github.com>
2024-07-23 18:44:38 +0300

    Add bottom menu bar
    Added mockup bottom menu bar for adding new functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import { Redirect, Stack } from "expo-router";
 
-export default function Index() {
+const StartPage = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <Redirect href="/home" />
+  )
 }
+
+export default StartPage;
---
+++ start.sh
@@ -0,0 +1,4 @@
+npx create-expo-app@latest RadioApp
+cd RadioApp
+touch tsconfig.json
+npx expo
